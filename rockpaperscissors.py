9fde0a6ae1442d15a9e6503c31a5a94628b05b8b
Bibek <study.bibekshrestha@gmail.com>
2024-09-20 11:33:32 +0300

    added comments

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,25 +2,33 @@ import random
 
 print(("Welcome to the rock, paper, scissors game."))
 
+# win counts
 user_wins = 0
 computer_wins = 0
 
+# list
 options = ["rock", "paper", "scissors"]
 
+# while loop
 while True:
     user_input = input("Type rock, paper or scissors. If you wish to quit please enter 'q'. ").strip().lower()
+    
+    # break the loop if user enters 'q'
     if user_input == "q":
         break
 
+    # if user inputs something other than the program intends, continue the loop
     if user_input not in options:
         continue
 
+    # generating random number between 0 to 2
     random_number = random.randint(0,2)
-
+    # associating randomly generated number to the options list
     computer_pick = options[random_number]
 
     print("Computer picked", computer_pick + ".")
 
+    # if-loop to check wins and draws
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         user_wins += 1
@@ -36,6 +44,7 @@ while True:
         print("You lost!")
         computer_wins += 1
 
+# when the loop ends user is presented with total number of wins.
 print("\nYou won", user_wins, "times.")
 print("Computer won", computer_wins, "times.")
 print("Thank you for playing!!\n")
